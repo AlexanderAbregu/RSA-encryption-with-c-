8e57e396abc715057a286f2c096bb85a2d7918b1
Alexander Armua <Alexander Armua>
2016-06-22 15:04:45 -0300

    Cambios finales

--- mainRSA.cpp
+++ mainRSA.cpp
@@ -1,11 +1,10 @@
 #include <iostream> 
 #include <windows.h> //Agregado para cambiar el color
-#include <conio.h> //Para el getch...
 #include <cmath> //Funciones matematicas: sqrt
 
 /*
  * Programar un encriptador de palabras mediante el metodo RSA,
- * o dos metodos de encriptacion simples (en caso de no querer usar RSA);
+ * o dos metodos de unsigned long long simples (en caso de no querer usar RSA);
  * donde dado un programa se muestre el mismo encriptado, con la opcion
  * de recuperar el mensaje original
  * */
@@ -13,7 +12,7 @@
 using namespace std;
 
 //Cambiar el color de las letras
-void color(int X){ 
+void color( int X ){ 
 	SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),X); 
 	/* 
 		0 = Negro 
@@ -31,30 +30,29 @@ void color(int X){
 
 //Se hace uso del algoritmo de Euclides para averiguar el MCD de dos numeros. Este metodo fue creado por el matematico griego Euclides.
 //https://es.wikipedia.org/wiki/Algoritmo_de_Euclides
-long mcdEuclides(long n1, long n2){
+long mcdEuclides( long n1, long n2 ){
 	long maximo, minimo, resto;
 	
-	//Identificamos el maximo y el minimo de los parametros.
-	if(n1>=n2){
-		maximo=n1;
-		minimo=n2;
+	if( n1 >= n2 ){
+		maximo = n1;
+		minimo = n2;
 	}else{
-		maximo=n2;
-		minimo=n1;
+		maximo = n2;
+		minimo = n1;
 	}
 	
-	while(minimo!=0){
-		resto=maximo%minimo;
-		maximo=minimo;
-		minimo=resto;
+	while( minimo != 0 ){
+		resto = ( maximo % minimo );
+		maximo = minimo;
+		minimo = resto;
 	}
 	
 	return maximo;
 }
 
 //Retorna true si el numero que le pasamos es par.
-bool esPar(long numero){
-	if(numero%2==0){
+bool esPar( long numero ){
+	if( ( numero % 2 ) == 0 ){
 		return true;
 	}else{
 		return false;
@@ -62,9 +60,9 @@ bool esPar(long numero){
 }
 
 //Por tentativa http://es.wikihow.com/saber-si-un-n%C3%BAmero-es-primo
-bool esPrimo(long n1){
+bool esPrimo( long n1 ){
 	//TODOS los numeros pares a excepcion del dos son compuestos.
-	if(esPar(n1) && n1!=2){
+	if( esPar( n1 ) && n1 != 2 ){
 		return false;
 	}else{
 		/*
@@ -72,16 +70,16 @@ bool esPrimo(long n1){
 		 * sqrt retorna la raiz cuadrada.
 		 * Se divide hasta la raiz cuadrada del numero ya que dividir por los numeros siguientes seria redundante. Ejemplo:
 		 * Analizar 100:
-		 * 1x100=100  -  2x50=100  -  4x25=100  -  5x20=100  
-		 * 10x10=100 (10 es raiz de 100)
-		 * 20x5=100  -  25x4=100  -  50x2=100  -  100x1=100
-		 * Como se puede ver a partir de la raiz en adelante los factores se reinvierten y por eso se divide hasta la raiz cuadrada del numero en cuestion.
+		 * 1 x 100 = 100  -  2 x 50 = 100  -  4 x 25 = 100  -  5 x 20 = 100  
+		 * 10 x 10 = 100 ( 10 es raiz de 100 )
+		 * 20 x 5 = 100  -  25 x 4 = 100  -  50 x 2 = 100  -  100 x 1 = 100
+		 * Como se puede ver a partir de la raiz (10) en adelante los factores se reinvierten y por eso se divide hasta la raiz cuadrada del numero en cuestion.
 		 * */
-		for( long i = 3; i < ceil( sqrt( n1 ) ); i++ ){
+		for( int i = 3; i < ceil( sqrt( n1 ) ); i++ ){
 			//Si no es par
-			if(!esPar(i)){
+			if( !esPar( i ) ){
 				//Si el resto de dividir n1 por el iterador es 0 significa que n1 es un numero compuesto.
-				if(n1%i==0){
+				if( ( n1 % i ) == 0 ){
 					return false;
 				}
 			}
@@ -91,27 +89,22 @@ bool esPrimo(long n1){
 }
 
 //Retorna un numero primo mayor que el parametro pasado
-long damePrimo(long mayorQue){
-	long numeroPrimo=0, numero=mayorQue;
+long damePrimo( long mayorQue ){
+	long numeroPrimo = 0, numero = mayorQue;
 	do{
 		//Con cada repeticion al numero le sumo 1 para ir probando hasta encontrar el primo
 		numero++;
-		if(esPrimo(numero)){
-			numeroPrimo=numero;
+		if( esPrimo( numero ) ){
+			numeroPrimo = numero;
 		}
-	}while(numeroPrimo==0);
+	}while( numeroPrimo == 0 );
 	 
 	return numeroPrimo;
 }
  
 //Obtener numero aleatorio entre dos rangos definidos
-long numeroAleatorioEntre(long minimo, long maximo){
-	return (minimo+rand()%((maximo+1)-minimo));
-}
- 
-//Obtener numero aleatorio mayor que otor numero
-long numeroAleatorioMayorQue(long minimo){
-	return (minimo+rand());
+long numeroAleatorioEntre( long minimo, long maximo ){
+	return ( minimo + ( rand() % ( ( maximo + 1 ) - minimo ) ) );
 }
 
 //Algoritmo de euclides extendido
@@ -122,7 +115,7 @@ long* algEucExtendido( long n1, long n2 ){
 		array[1] = 1; 
 		array[2] = 0;  
 	}else{
-		while(n2>0){
+		while( n2 > 0 ){
 			q = ( n1 / n2 ); 
 			r = n1 - q * n2; 
 			x = x2 - q * x1; 
@@ -134,16 +127,19 @@ long* algEucExtendido( long n1, long n2 ){
 			y2 = y1;             
 			y1 = y; 
 		}
-		array[0] = n1;   // mcd (n1,n2)
+		array[0] = n1;   // mcd ( n1, n2 )
 		array[1] = x2;   // x
 		array[2] = y2;   // y
 	}
 	return array;
 }
 
-//Inverso de un numero en un modulo
+/*
+	Dados los numeros enteros a y n > 0, el inverso multiplicativo de "a" modulo "n" 
+	esta dado por el numero entero "y" que pertenece a Zn, tal que a x y = 1(mod n).
+*/
 long Inverso_Zn( long a, long n ){
-	long* ptr,	array[3];
+	long* ptr, array[3];
 	ptr = algEucExtendido( n, a );
  
 	array[0] = *ptr;
@@ -160,7 +156,10 @@ long Inverso_Zn( long a, long n ){
 	}  
 }
 
-//Exponenciacion de un numero en un modulo
+/*
+	Exponenciacion modular
+	Emplea una representacion binaria de un numero entero "K".
+*/
 unsigned long long  Exponenciacion_Zn(unsigned long long  a,unsigned long long  k,unsigned long long  n){
 	// convertimos "k" a binario
 	unsigned long long numero = k;
@@ -168,12 +167,12 @@ unsigned long long  Exponenciacion_Zn(unsigned long long  a,unsigned long long  
 	unsigned long long bin[300];
 	unsigned long long  ind = 0;
 	while( numero >= 2 ){
-	bin[ind++] = numero % 2;
-	numero/=2;      
+		bin[ind++] = numero % 2;
+		numero /= 2;      
 	}     
 	bin[ind] = numero;
 	unsigned long long  tam = ind + 1;
-	// for(long i=0;i<tam;i++)
+	// for(unsigned long long i=0;i<tam;i++)
 	// cout<<bin[i]<<endl;     
 	/////////////////////////////   
       
@@ -190,7 +189,7 @@ unsigned long long  Exponenciacion_Zn(unsigned long long  a,unsigned long long  
 		}
 	// cout<<"b :"<<b<<endl;   
 	}
-return b;
+	return b;
 }
 
 /*
@@ -200,32 +199,36 @@ return b;
 	[2] = Mensaje encriptado.
 */
 long*encriptar(long mensaje){
-	long array[3], cifrado=0;
-
-	//Generacion del par de claves
-	//Paso 1: Generar dos numeros primos aleatorios GRANDES con el mismo tamaño.
-	long primoUno = damePrimo( numeroAleatorioEntre( 50000, 99999 )	);
-	long primoDos = damePrimo( numeroAleatorioEntre( 10000, 49999 )	);
+	long array[3], cifrado=0, productoPrimos=0, exponenteClavePrivada=0;
 	
-	//Paso 2: Se calcula el producto entre los dos numeros primos.
-	long productoPrimos = ( primoUno * primoDos );  // n
-	array[0] = productoPrimos;
-	
-	//Paso 3: Creacion de la clave publica.
-	long clavePublica = ( ( primoUno - 1 ) * ( primoDos - 1 ) ); // d   p(n) fi
-	
-	//Paso 4: Seleccionamos un entero aleatorio 'e' tal que mcd('e',clavePublica) = 1   y   1 < 'e' < clavePublica
-	long e;
-	do{
-		e = numeroAleatorioEntre( 2, clavePublica );
-	}while( mcdEuclides( e, clavePublica ) != 1 );
-	
-	long exponenteClavePrivada = Inverso_Zn(e,clavePublica); // d
-	array[1]=exponenteClavePrivada;
-	
-	cifrado=Exponenciacion_Zn(mensaje,e,productoPrimos);
-	
-	array[2]=cifrado;
+	while( cifrado == 0 || productoPrimos < 0 || exponenteClavePrivada < 0 ){
+		//Generacion del par de claves
+		//Paso 1: Generar dos numeros primos aleatorios GRANDES con el mismo tamaño.
+		long primoUno = damePrimo( numeroAleatorioEntre( 50000, 99999 )	);
+		long primoDos = damePrimo( numeroAleatorioEntre( 10000, 49999 )	);
+		
+		//Paso 2: Se calcula el producto entre los dos numeros
+		productoPrimos = ( primoUno * primoDos );
+		array[0] = productoPrimos;
+		
+		//Paso 3: Creacion de la clave publica.
+		long clavePublica = ( ( primoUno - 1 ) * ( primoDos - 1 ) );
+		
+		//Paso 4: Seleccionamos un entero aleatorio 'e' tal que mcd('e',clavePublica) = 1   y   1 < 'e' < clavePublica
+		long e;
+		do{
+			e = numeroAleatorioEntre( 2, clavePublica );
+		}while( mcdEuclides( e, clavePublica ) != 1 );
+		
+		//Inverso Multiplicativo de "a" modulo "n"
+		exponenteClavePrivada = Inverso_Zn(e,clavePublica);
+		array[1]=exponenteClavePrivada;
+		
+		//Exponenciacion Modular
+		cifrado=Exponenciacion_Zn(mensaje,e,productoPrimos);
+		
+		array[2]=cifrado;
+	}
 	
 	return array;
 }
@@ -255,7 +258,8 @@ int main(){
  		
 	long* clavesEncriptacion=0, clavePublica=0, clavePrivada=0, cifrado=0;
 	
-	long mensaje=0, respuesta=0;
+	long mensaje=0;
+	int respuesta=0;
 	
 	do{
 			color(7);
@@ -308,8 +312,6 @@ int main(){
 	cout<<"Adios"<<endl;
 	
 	//Final de los programas
-    getch();
     return 0;
 	system("pause");
-
 }
